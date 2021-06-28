8dc59ca3ae219fe511d284cfb3903aa6927319d1
eladyaish <eladyi@cellcom.co.il>
2021-06-28 21:40:00 +0300

    first commit

---
+++ testApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace testApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
